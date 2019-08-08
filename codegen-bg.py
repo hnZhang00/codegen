ec6b15296813018845bb5f01f07d7ba3abd30e72
HnZhang <hnZhang0_0@163.com>
2019-08-08 18:05:57 +0800

    update

---
+++ codegen-bg.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import jinja2
+import json5
+import sys
+import getopt
+import glob
+import os
+import errno
+import execjs
+import utils
+
+
+class OptArg:
+    template_file = ''
+    code_file = ''
+    instance_file = ''
+    instance_file_type = ''
+
+    def __init__(self):
+        pass
+
+
+def underscore(s):
+    return s.replace(" ", "_").replace("-", "_")
+
+
+def get_opt_arg():
+    optarg = OptArg()
+    try:
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            "ht:c:i:",
+            ["template_file=", "code_file=", "instance_file="]
+        )
+    except getopt.GetoptError:
+        # print('-t <template_file> -i <instance_file> -c <code_file>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('Usage: codegen -t <template_file> -i <instance_file> -c <code_file>')
+            sys.exit()
+        elif opt in ("-t", "--template_file"):
+            optarg.template_file = arg
+        elif opt in ("-c", "--code_file"):  # output code file
+            optarg.code_file = arg
+        elif opt in ("-i", "--instance_file"):  # dir/* dir/*.txt dir/file.txt
+            optarg.instance_file = arg
+        elif opt in ("-o", "--instance_file_type"):  # TODO js or json5 
+            optarg.instance_file_type = arg
+    return optarg
+
+
+def read_config(instance_file, schema_type):
+    content = open(instance_file, 'r').read()
+    print (content)
+    if 'js' == schema_type:
+        ctx = execjs.compile(content)
+        return ctx.eval('__config__')
+    return json5.loads(content)
+
+
+def write_to(file_path, content):
+    # print(os.path.dirname(file_path))
+    file_dir = os.path.dirname(file_path)
+    if not os.path.exists(file_dir):
+        try:
+            os.makedirs(file_dir)
+        except OSError as err:  # Guard against race condition
+            if err.errno != errno.EEXIST:
+                raise
+
+    f = open(file_path, "w")
+    try:
+        f.write(content)
+    finally:
+        f.close()
+
+
+if __name__ == '__main__':
+    opt_arg = get_opt_arg()
+
+    # init environment
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
+    print (env)
+    env.filters['underscore'] = underscore
+    env.filters['pascalcase'] = utils.pascalcase
+    env.filters['camelcase'] = utils.camelcase
+
+    # get template
+    template = env.get_template(opt_arg.template_file)
+
+    # list all file match config path
+    for e in glob.glob(opt_arg.instance_file):
+        config = read_config(e, opt_arg.instance_file_type)
+        # print (config)
+        out = template.render(config)
+
+        out_path = env.from_string(opt_arg.code_file).render(config)
+        if '' == out_path:
+            raise ValueError('output path is empty')
+        # print (out_path)
+
+        write_to(out_path, out)
+
+
+
---
+++ codegen.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import jinja2
+import json5
+import sys
+import getopt
+import glob
+import os
+import errno
+import execjs
+import utils
+
+
+class OptArg:
+    router_templ_file_path = './config/router.tmpl'
+    view_templ_file_path = './config/view.tmpl'
+    config_file_path = './config/config.json5'
+    output_file_path = './src/'
+
+    def __init__(self):
+        pass
+
+
+def underscore(s):
+    return s.replace(" ", "_").replace("-", "_")
+
+
+def get_opt_arg():
+    optarg = OptArg()
+    try:
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            "hc:o:",
+            ["config_file=", "output_file_path="]
+        )
+    except getopt.GetoptError:
+        print('-c <code_file> -o <output_file_path>')
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('Usage: codegen -t <template_file> -c <config_file> -o <output_file_path>')
+            sys.exit()
+        elif opt in ("-c", "--config_file"):  # config file path
+            optarg.config_file_path = arg
+        elif opt in ("-o", "--output_file_path"):  # output file path
+            optarg.output_file_path = arg
+    return optarg
+
+
+def read_config(config_file_path):
+    content = open(config_file_path, 'r').read()
+    return json5.loads(content)
+
+
+def write_to(file_path, content):
+    # print(os.path.dirname(file_path))
+    file_dir = os.path.dirname(file_path)
+    if not os.path.exists(file_dir):
+        try:
+            os.makedirs(file_dir)
+        except OSError as err:  # Guard against race condition
+            if err.errno != errno.EEXIST:
+                raise
+
+    f = open(file_path, "w")
+    try:
+        f.write(content)
+    finally:
+        f.close()
+
+
+if __name__ == '__main__':
+    opt_arg = get_opt_arg()
+
+    # init environment
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
+    env.filters['underscore'] = underscore
+    env.filters['pascalcase'] = utils.pascalcase
+    env.filters['camelcase'] = utils.camelcase
+
+    # get template
+    def get_file_templ(file_path):
+        return env.get_template(file_path)
+    
+    
+    def render_file(_template, _data, _path):
+        output_file = _template.render(_data)
+        output_file_path = env.from_string(_path).render(_data)
+        if '' == output_file_path:
+            raise ValueError('output path is empty')
+        write_to(output_file_path, output_file)
+    
+
+    router_template = get_file_templ(opt_arg.router_templ_file_path)
+    view_template = get_file_templ(opt_arg.view_templ_file_path)
+        
+    config = read_config(opt_arg.config_file_path)
+
+    render_file(router_template, config, opt_arg.output_file_path + 'router.js')
+
+    for views in config.values():
+        for view in views:
+            view_name = ''
+            for key, value in view.items():
+                if key == 'name':
+                    view_name = value
+            render_file (view_template, view, opt_arg.output_file_path + view_name +'/index.vue')
+
+
