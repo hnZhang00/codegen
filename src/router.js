ecb79d07c57fff33597b88f023613283966fb053
HnZhang <hnZhang0_0@163.com>
2019-08-14 17:01:34 +0800

    update

--- src/router.js
+++ src/router.js
@@ -44,65 +44,202 @@ export const constantRoutes = [
   },
 
   {
-    path: '/view1',
-    component: () => import('@/views/view1/index'),
-    hidden: false
+    path: '/',
+    component: Layout,
+    redirect: '/home',
+    children: [
+      {
+        path: 'home',
+        name: 'Home',
+        component: () => import('@/views/home/index'),
+        meta: { title: '首页', icon: 'dashboard' }
+      }
+    ]
   },
+
   {
-    path: '/view1/:id',
-    component: () => import('@/views/view1/detail'),
-    hidden: true
+    path: '/view1',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        name: 'view1',
+        component: () => import('@/views/view1/index'),
+        meta: { title: '页面1', icon: '' },
+        children: [
+          {
+            path: ':id',
+            hidden: true,
+            component: () => import('@/views/view1/detail/index'),
+            name: 'view1_detail',
+            meta: { title: '页面1详情', icon: '' }
+          }
+        ]
+      }
+    ]
   },
 
   {
     path: '/view2',
-    component: () => import('@/views/view2/index'),
-    hidden: false
-  },
-  {
-    path: '/view2/:id',
-    component: () => import('@/views/view2/detail'),
-    hidden: true
+    component: Layout,
+    children: [
+      {
+        path: '',
+        name: 'view2',
+        component: () => import('@/views/view2/index'),
+        meta: { title: '页面2', icon: '' },
+        children: [
+          {
+            path: ':id',
+            hidden: true,
+            component: () => import('@/views/view2/detail/index'),
+            name: 'view2_detail',
+            meta: { title: '页面2详情', icon: '' }
+          }
+        ]
+      }
+    ]
   },
 
   {
     path: '/view3',
-    component: () => import('@/views/view3/index'),
-    hidden: false
+    component: Layout,
+    children: [
+      {
+        path: '',
+        name: 'view3',
+        component: () => import('@/views/view3/index'),
+        meta: { title: '页面3', icon: '' },
+        children: [
+          {
+            path: ':id',
+            hidden: true,
+            component: () => import('@/views/view3/detail/index'),
+            name: 'view3_detail',
+            meta: { title: '页面3详情', icon: '' }
+          }
+        ]
+      }
+    ]
   },
+
+  /**
   {
-    path: '/view3/:id',
-    component: () => import('@/views/view3/detail'),
-    hidden: true
+    path: '/',
+    component: Layout,
+    redirect: '/dashboard',
+    children: [{
+      path: 'dashboard',
+      name: 'Dashboard',
+      component: () => import('@/views/dashboard/index'),
+      meta: { title: 'Dashboard', icon: 'dashboard' }
+    }]
   },
 
+  {
+    path: '/example',
+    component: Layout,
+    redirect: '/example/table',
+    name: 'Example',
+    meta: { title: 'Example', icon: 'example' },
+    children: [
+      {
+        path: 'table',
+        name: 'Table',
+        component: () => import('@/views/table/index'),
+        meta: { title: 'Table', icon: 'table' }
+      },
+      {
+        path: 'tree',
+        name: 'Tree',
+        component: () => import('@/views/tree/index'),
+        meta: { title: 'Tree', icon: 'tree' }
+      }
+    ]
+  },
 
-  // {
-  //   path: '/nested',
-  //   component: Layout,
-  //   redirect: '/nested/menu1',
-  //   name: 'Nested',
-  //   meta: {
-  //     title: 'Nested',
-  //     icon: 'nested'
-  //   },
-  //   children: [
-  //     {
-  //       path: 'menu1',
-  //       component: () => import('@/views/nested/menu1/index'), // Parent router-view
-  //       name: 'Menu1',
-  //       meta: { title: 'Menu1' },
-  //       children: [
-  //         {
-  //           path: 'menu1-1',
-  //           component: () => import('@/views/nested/menu1/menu1-1'),
-  //           name: 'Menu1-1',
-  //           meta: { title: 'Menu1-1' }
-  //         },
-  //       ]
-  //     }
-  //   ]
-  // },
+  {
+    path: '/form',
+    component: Layout,
+    children: [
+      {
+        path: 'index',
+        name: 'Form',
+        component: () => import('@/views/form/index'),
+        meta: { title: 'Form', icon: 'form' }
+      }
+    ]
+  },
+
+  {
+    path: '/nested',
+    component: Layout,
+    redirect: '/nested/menu1',
+    name: 'Nested',
+    meta: {
+      title: 'Nested',
+      icon: 'nested'
+    },
+    children: [
+      {
+        path: 'menu1',
+        component: () => import('@/views/nested/menu1/index'), // Parent router-view
+        name: 'Menu1',
+        meta: { title: 'Menu1' },
+        children: [
+          {
+            path: 'menu1-1',
+            component: () => import('@/views/nested/menu1/menu1-1'),
+            name: 'Menu1-1',
+            meta: { title: 'Menu1-1' }
+          },
+          {
+            path: 'menu1-2',
+            component: () => import('@/views/nested/menu1/menu1-2'),
+            name: 'Menu1-2',
+            meta: { title: 'Menu1-2' },
+            children: [
+              {
+                path: 'menu1-2-1',
+                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-1'),
+                name: 'Menu1-2-1',
+                meta: { title: 'Menu1-2-1' }
+              },
+              {
+                path: 'menu1-2-2',
+                component: () => import('@/views/nested/menu1/menu1-2/menu1-2-2'),
+                name: 'Menu1-2-2',
+                meta: { title: 'Menu1-2-2' }
+              }
+            ]
+          },
+          {
+            path: 'menu1-3',
+            component: () => import('@/views/nested/menu1/menu1-3'),
+            name: 'Menu1-3',
+            meta: { title: 'Menu1-3' }
+          }
+        ]
+      },
+      {
+        path: 'menu2',
+        component: () => import('@/views/nested/menu2/index'),
+        meta: { title: 'menu2' }
+      }
+    ]
+  },
+
+  {
+    path: 'external-link',
+    component: Layout,
+    children: [
+      {
+        path: 'https://panjiachen.github.io/vue-element-admin-site/#/',
+        meta: { title: 'External Link', icon: 'link' }
+      }
+    ]
+  },
+   */
 
   // 404 page must be placed at the end !!!
   { path: '*', redirect: '/404', hidden: true }
